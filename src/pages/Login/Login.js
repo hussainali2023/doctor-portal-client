3518e5d9e5c318b4c6a4771582d418ffa92c5bed
hussain2023 <iamhussainali2@gmail.com>
2022-11-13 21:39:41 +0530

    simple login form design

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,29 +1,55 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+
+  const handleLogin = (data) => {
+    console.log(data);
+  };
   return (
     <div className=" h-[800px] flex justify-center items-center">
-      <div>
-        <h2 className=" text-4xl font-semibold">Login</h2>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+      <div className=" w-96 p-6">
+        <h2 className=" text-xl font-semibold text-center">Login</h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
-              {...register("firstName")}
-              type="text"
-              className="input input-bordered w-full max-w-xs"
+              {...register("email")}
+              type="email"
+              className="input input-bordered w-full"
             />
           </div>
-
-          <textarea {...register("aboutYou")} placeholder="About you" />
-          <p>{data}</p>
-          <input type="submit" />
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              {...register("password")}
+              type="password"
+              className="input input-bordered w-full"
+            />
+            <label className="label">
+              <span className="label-text">Forget Password ?</span>
+            </label>
+          </div>
+          <input
+            className=" btn btn-accent text-white font-semibold w-full"
+            type="submit"
+            value={"submit"}
+          />
         </form>
+        <p className=" text-secondary">
+          New to Doctors Portal{" "}
+          <Link className=" text-white" to="/signup"></Link>
+          <div className="divider">OR</div>
+          <button className=" btn btn-outline uppercase w-full">
+            Continue With Google
+          </button>
+        </p>
       </div>
     </div>
   );
